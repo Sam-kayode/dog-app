8beff938e4325e0b8867ef82037df7f77933b654
Marvellous Sam-kayode <marvelloussamkayode@gmail.com>
2023-02-07 19:05:05 +0100

    Add initial vue 3 starter boilerplate

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+
+  </div>
+  <RouterView />
+</template>
+<script>
+// import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+<style scoped lang="scss">
+@import './assets/scss/variables'
+</style>
